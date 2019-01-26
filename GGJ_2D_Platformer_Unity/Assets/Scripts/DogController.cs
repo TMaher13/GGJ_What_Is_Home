72c74d6ce4641e1a2c52a4cdffeb0a65e4502b6b
Tariq <trakha10899@gmail.com>
2019-01-26 11:42:38 -0500

    sprite additions and dogController script. This is not finished, currently work in progress

--- GGJ_2D_Platformer_Unity/Assets/Scripts/DogController.cs
+++ GGJ_2D_Platformer_Unity/Assets/Scripts/DogController.cs
@@ -8,16 +8,21 @@ public class DogController : MonoBehaviour
 {
 
     public Transform[] points;
-    private int destPoint = 0;
-    private NavMeshAgent agent;
+    private int destPoint;
+    //private NavMeshAgent agent;
+    public float speed;
+    Transform currentPoint;
 
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        //agent = GetComponent<NavMeshAgent>();
 
-        // Disabling auto-braking allows for continuous movement
-        agent.autoBraking = false;
+        // makes continuous movement
+        //agent.autoBraking = false;
+
+        destPoint = 0;
+        currentPoint = points[destPoint];
 
         GotoNextPoint();
     }
@@ -25,24 +30,43 @@ public class DogController : MonoBehaviour
 
     void GotoNextPoint()
     {
-        // Returns if no points have been set up
+        // if no points have been made
         if (points.Length == 0)
             return;
 
-        // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
+        // go to selected point
+        //agent.destination = points[destPoint].position;
 
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        //move to point
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        // choose next point, or cycle through
+        //destPoint = (destPoint + 1) % points.Length;
+
+        if(Vector3.Distance(transform.position, currentPoint.position) <.1f)
+        {
+            if (destPoint + 1 < points.Length)
+            {
+                destPoint++;
+            }
+            else
+            {
+                destPoint = 0;
+            }
+            currentPoint = points[destPoint];
+        }
+
+        Vector3 pointDir = currentPoint.position - transform.position;
+        float angle = Mathf.Atan2(pointDir.y, pointDir.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
     }
 
 
     void Update()
     {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        // choose the next point as the dog gets close to the first point
+        //if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        //  GotoNextPoint();
     }
 }
