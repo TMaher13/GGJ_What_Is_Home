816dec70560707f31871b5188fcdb0fcc0e9eb99
Tariq <trakha10899@gmail.com>
2019-01-25 21:50:34 -0500

    i added a folder and a cube and a script. they are nice

--- GGJ_2D_Platformer_Unity/Assets/PlayerController.cs
+++ GGJ_2D_Platformer_Unity/Assets/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Monobehaviour {
+public class Player : MonoBehaviour {
 
   private float moveInput;
 
