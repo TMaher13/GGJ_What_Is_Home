57ff56ec85613aae5f591eda72e8df59390bee09
christianschroederVT <cschroeder1338@gmail.com>
2019-01-27 10:37:32 -0500

    uncommented code for animations to work.

--- GGJ_2D_Platformer_Unity/Assets/Scripts/PlayerController.cs
+++ GGJ_2D_Platformer_Unity/Assets/Scripts/PlayerController.cs
@@ -22,11 +22,13 @@ public class PlayerController : MonoBehaviour {
   public string startPoint;
 
   private Rigidbody2D rb;
-  //private Animator anim;
+  private Animator anim;
 
   void Start () {
-    //anim = GetComponent<Animator>();
+    anim = GetComponent<Animator>();
+        
     rb = GetComponent<Rigidbody2D>();
+
     //pos = transform.position;
     //DontDestroyOnLoad(transform.gameObject);
     if(!playerExists) {
@@ -45,15 +47,15 @@ public class PlayerController : MonoBehaviour {
     // Basic movement of player
     if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
       //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*moveSpeed, rb.velocity.y);
-      //isMoving = true;
-      //lastMove = new Vector2(0f, Input.GetAxisRaw("Horizontal"));
+      isMoving = true;
+      lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
       transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime, 0f, 0f));
     }
 
     if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
       //rb.velocity = new Vector2(rb.velocity.x, Input.GetAxisRaw("Vertical")*moveSpeed);
-      //isMoving = true;
-      //lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+      isMoving = true;
+      lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
       transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime, 0f));
     }
 
@@ -66,13 +68,13 @@ public class PlayerController : MonoBehaviour {
       rb.velocity = new Vector2(rb.velocity.x, 0f);
     }
 
-    /*
+    
     anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
     anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-    anim.setBool("isMoving", isMoving);
+    anim.SetBool("isMoving", isMoving);
     anim.SetFloat("LastMoveX", lastMove.x);
     anim.SetFloat("LastMoveY", lastMove.y);
-    */
+    
   }
 
 
