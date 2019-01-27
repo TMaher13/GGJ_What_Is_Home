b368108198795da69d961fa20e361837bf8bf62a
Joey Rodgers <jdr14@vt.edu>
2019-01-27 13:29:03 -0500

    Sibling commit of the previous one.  Includes assets needed for the main menu.

---
+++ GGJ_2D_Platformer_Unity/Assets/VolumeValueChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour {
+
+    private AudioSource musicPlayer;
+
+    private float musicVolume = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+        musicPlayer = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // setting the volume option of the audio source should be equal to music volume
+        musicPlayer.volume = musicVolume;
+	}
+
+    // Method to be used by the slider game object
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
