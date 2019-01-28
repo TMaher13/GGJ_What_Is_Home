a043e0e4e9dd6dff53b031519706bf2a48bba097
TMaher13 <tomm13@vt.edu>
2019-01-28 00:35:43 -0500

    Updated camera to stay in bounds

--- GGJ_2D_Platformer_Unity/Assets/Scripts/CameraController.cs
+++ GGJ_2D_Platformer_Unity/Assets/Scripts/CameraController.cs
@@ -15,8 +15,16 @@ public class CameraController : MonoBehaviour {
   private Vector3 playerPos;
   public float moveSpeed; // How fast we want camera to move. 5 is roughly a good value (maybe a little less)
 
+  public BoxCollider2D boundBox;
+  private Vector3 minBounds;
+  private Vector3 maxBounds;
+
+  private Camera camera;
+  private float halfHeight;
+  private float halfWidth;
+
   void Start() {
-    DontDestroyOnLoad(transform.gameObject);
+    //DontDestroyOnLoad(transform.gameObject);
 
     if(!cameraExists) {
       cameraExists = true;
@@ -26,12 +34,35 @@ public class CameraController : MonoBehaviour {
     else { // To destroy duplicates
       Destroy(gameObject);
     }
+
+    minBounds = boundBox.bounds.min;
+    maxBounds = boundBox.bounds.max;
+
+    camera = GetComponent<Camera>();
+    halfHeight = camera.orthographicSize;
+    halfWidth = halfHeight * Screen.width / Screen.height;
   }
 
   void Update() {
 
     playerPos = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
-
     transform.position = Vector3.Lerp(transform.position, playerPos, moveSpeed * Time.deltaTime); // Camera follows the player but 6 to the right
+
+    if(boundBox == null) {
+      boundBox = FindObjectOfType<BoundsScript>().GetComponent<BoxCollider2D>();
+      minBounds = boundBox.bounds.min;
+      maxBounds = boundBox.bounds.max;
+    }
+
+    float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+    float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+    transform.position = new Vector3(clampedX, clampedY, transform.position.z);
   }
+
+  public void SetBounds(BoxCollider2D newBounds) {
+    boundBox = newBounds;
+    minBounds = boundBox.bounds.min;
+    maxBounds = boundBox.bounds.max;
+  }
+
 }
